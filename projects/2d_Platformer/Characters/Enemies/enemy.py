a9635326e92c22b52e5f89699f17fed44f700d58
Crowley55555 <lavrovartem0511@gmail.com>
2025-08-04 21:04:22 +0300

    Обновил портфолио

--- projects/2d_Platformer/Characters/Enemies/enemy.py
+++
@@ -1,5 +0,0 @@
-from Characters.character import Character
-
-class Enemy(Character):
-   def __init__(self, name, speed: float, strength: float):
-      super().__init__(name, speed, strength)
--- projects/2d_Platformer/custom_logging/__init__.py
+++
@@ -1,5 +0,0 @@
-from .logger import Logger
-from .interfaces import ILogger
-from .debug_logger import DebugLogger
-
-__all__ = ["Logger", "ILogger", "DebugLogger"]
